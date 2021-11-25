7ec906ff5172283756929c7223b9a691c4acb29d
pratibhapw23 <pratibha_wabale@persistent.com>
2021-11-25 18:39:50 +0530

    Used Web Client insted of Rest Template

--- book-catalog-service/src/main/java/com/springbootProjects/bookcatalogservice/controller/BookCatalogcontroller.java
+++ book-catalog-service/src/main/java/com/springbootProjects/bookcatalogservice/controller/BookCatalogcontroller.java
@@ -4,10 +4,12 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.springbootProjects.bookcatalogservice.domain.Book;
 import com.springbootProjects.bookcatalogservice.domain.CatalogItem;
@@ -17,10 +19,15 @@ import com.springbootProjects.bookcatalogservice.domain.Ratings;
 @RequestMapping("/catalog")
 public class BookCatalogcontroller {
 
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Autowired
+	private WebClient.Builder webClientBuilder;
+	
 	@RequestMapping("/{userid}")
 	public List<CatalogItem> setCatalog(@PathVariable("userid") String userid){
-		
-		RestTemplate restTemplate=new RestTemplate();
+				
 		//
 		//1. Get all Book ids
 		
@@ -32,8 +39,17 @@ public class BookCatalogcontroller {
 				);
 		
 		return ratings.stream().map(rating -> {
-			Book book=restTemplate.getForObject("http://localhost:8082/books/" + rating.getBookid(), Book.class);
-		return new CatalogItem(book.getName(),"Author1","9/10/1990", rating.getRating());
+			//Book book=restTemplate.getForObject("http://localhost:8082/books/" + rating.getBookid(), Book.class);
+		
+			Book book=webClientBuilder.build()
+			.get()
+			.uri("http://localhost:8082/books/" + rating.getBookid())
+			.retrieve()
+			.bodyToMono(Book.class)
+			.block();
+			
+			return new CatalogItem(book.getName(),"Author1","9/10/1990", rating.getRating());
+					
 	})
 	.collect(Collectors.toList());
 				
