0d45fbb99ad3a404c6265c88c1cd58c6736183a4
pratibhapw23 <pratibha_wabale@persistent.com>
2021-11-25 18:39:06 +0530

    Created Bean of Web Client

--- book-catalog-service/src/main/java/com/springbootProjects/bookcatalogservice/BookCatalogServiceApplication.java
+++ book-catalog-service/src/main/java/com/springbootProjects/bookcatalogservice/BookCatalogServiceApplication.java
@@ -2,10 +2,26 @@ package com.springbootProjects.bookcatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class BookCatalogServiceApplication {
 
+	@Bean
+	public RestTemplate restTemplate() {
+		
+		return new RestTemplate();
+		
+	}
+	
+	@Bean
+	public WebClient.Builder getWebClient(){
+		
+		return WebClient.builder();
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BookCatalogServiceApplication.class, args);
 	}
